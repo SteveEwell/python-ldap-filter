dd868b9a8faa41ea571970e824a86568d1e60fcf
Stephen Ewell <steve@ewell.io>
2018-02-26 08:35:41 -0800

    Updated docstring in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+"""
+python-ldap-filter
+==================
+
+**Build and generate LDAP filters**
+
+A Python utility library for working with Lightweight Directory Access
+Protocol (LDAP) filters.
+
+This project is a port of the
+`node-ldap-filters <https://github.com/tapmodo/node-ldap-filters>`__ and
+implements many of the same APIs. The filters produced by the library
+are based on `RFC 4515 <https://tools.ietf.org/html/rfc4515>`__.
+
+Links
+-----
+
+`GitHub <https://github.com/SteveEwell/python-ldap-filter>`_
+
+"""
+
 import os
 import sys
 from setuptools import setup
