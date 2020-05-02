43b8359e655af64b36b32efab9a20325e48122a6
Steve <darksle@gmail.com>
2020-05-02 15:14:04 -0400

    attempt to resolve CI issues

--- setup.py
+++ setup.py
@@ -50,6 +50,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='ldap filter rfc4515 utility development',
     packages=['ldap_filter'],
